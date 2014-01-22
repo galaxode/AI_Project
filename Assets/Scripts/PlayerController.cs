e20780076419ea175a803d163448e3172a595b3a
Omer Useche <madrusec@gmail.com>
2014-01-22 03:14:25 -0500

    Reverting after test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () 
+	{
+		controller = GetComponent<CharacterController>;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0, Input.getAxisRaw ("Veritical"));
+
+		//transform.rotation = Quartenion.lookRotation (input);
+	}
+}
